f4076efcf7c34a2f21ea923a46ae1ae4d2ed6a70
Adam Franks <52870365+adfranks@users.noreply.github.com>
2019-10-09 00:07:50 -0400

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,15 @@
+/* Play the selected music track, but first pause any music
+being played already. */
+var i, audioEl, sourceMp3, sourceOgg,
+    musicAnchor = document.getElementsByClassName("music-anchor");
+
+for (i = 0; i < musicAnchor.length; i++) {musicAnchor[i].addEventListener("click", 
+    function(event) { 
+        audioEl = event.target.parentElement.parentElement.previousElementSibling;
+        sourceMp3 = audioEl.getElementsByClassName("mp3-src")[0];
+        sourceOgg = audioEl.getElementsByClassName("ogg-src")[0];
+        sourceMp3.src = "audio//" + event.target.id + ".mp3";  
+        sourceOgg.src = "audio//" + event.target.id + ".ogg";  
+        audioEl.load(); audioEl.play(); 
+    });
+}
